51a8c851ac8bd9ce734f0364dfc21e6ef8043e18
Sky Blue <dawadiaakash@gmail.com>
2025-04-11 08:36:00 +0545

    caesar cipher

--- part1.rb
+++ part1.rb
@@ -9,3 +9,20 @@ elsif x > y
 else
   puts "Y is greater"
 end
+
+
+
+# caesar cipher
+def caesar_cipher(str, shift)
+    encryption = ""
+    str.each_char do |char|
+        if char =~ /[A-Za-z]/
+            check = char.ord < 91 ?  'A'.ord : 'a'.ord
+            encryption += ((char.ord - check + shift) % 26 + check).chr
+        else    
+            encryption += char
+        end
+    end
+    encryption
+end
+puts caesar_cipher("Hello, AAKASH!", 3)
