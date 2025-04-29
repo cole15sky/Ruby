e22b7c0231529a3b9f1db5402998d033a93facc5
Sky Blue <dawadiaakash@gmail.com>
2025-04-29 18:19:36 +0545

    variable use

--- part2.rb
+++ part2.rb
@@ -4,3 +4,7 @@
 # print '3 THIS IS AAKASH HERE'
 
 
+charatcer_name = 'Elton John'
+character_age = '76'
+puts ("It's not a sacrifice, no sacrifice at all by " + charatcer_name)
+puts(charatcer_name +  'is' + character_age + " years old")
