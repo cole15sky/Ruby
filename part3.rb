fd2a1396d7f4b135997199f5630a2fcee32d4620
Sky Blue <dawadiaakash@gmail.com>
2025-04-29 18:41:17 +0545

    Data Types

---
+++ part3.rb
@@ -0,0 +1,37 @@
+# name = "Billy Joel"
+# age = 62
+# fav_album = "52nd Street"
+# rating = 8.9
+# ismale = true
+
+# puts name + " is " + age.to_s + " years old"
+# puts name[0,3] 
+# puts name.index("l")
+# puts name.upcase
+
+
+
+# puts 99+1
+# puts 99-1
+# puts 99*1
+# puts 99/1
+# puts 99%4
+# puts 99**1
+# puts 99.0/1
+# puts 99.0/1.0
+
+num = 99.89
+puts num.round()
+puts num.ceil()
+puts num.floor()
+puts num.abs()
+puts num.to_i
+puts num.to_s
+puts num.class
+
+
+puts Math.log(100)
+puts Math.sqrt(100)
+puts Math.sin(90)
+puts 10/7
+puts 10/7.0
