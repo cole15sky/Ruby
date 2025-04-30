21a47836e1561510ff6b0853457645e753dc02df
Sky Blue <dawadiaakash@gmail.com>
2025-04-30 12:33:52 +0545

    test mad libs

---
+++ part5.rb
@@ -0,0 +1,9 @@
+puts "Enter the name:"
+name = gets.chomp()
+puts "Enter the subject:"
+subject = gets.chomp()
+
+puts ("Hey Little " + name)
+puts ( subject + " gotta so much love..")
+puts ("ohhh Little " + name)
+
