6167e6d748911c91b9258e347802a2f514fea501
Sky Blue <dawadiaakash@gmail.com>
2025-04-30 12:22:34 +0545

    Calculator basic logics

--- part4.rb
+++ part4.rb
@@ -1,6 +1,31 @@
-puts "Enter your Name:"
-name = gets.chomp()
-puts "Enter your Age:"
-age = gets.chomp()
-puts ("Hello " + name + " , How are you doing today?")
-puts ("Hola " + name + ", you are " + age + " years old")
+# puts "Enter your Name:"
+# name = gets.chomp()
+# puts "Enter your Age:"
+# age = gets.chomp()
+# puts ("Hello " + name + " , How are you doing today?")
+# puts ("Hola " + name + ", you are " + age + " years old")
+
+
+# Calculator 
+
+
+
+puts "Enter first number:"
+num1 = gets.chomp()
+puts "Enter second number:"
+num2 = gets.chomp()
+
+# integers
+puts (num1.to_i + num2.to_i) 
+# # floats
+puts (num1.to_f + num2.to_f) 
+
+
+# another way 
+puts "Enter first number:"
+num1 = gets.chomp().to_f
+puts "Enter second number:"
+num2 = gets.chomp().to_f
+
+puts (num1 + num2)
+
