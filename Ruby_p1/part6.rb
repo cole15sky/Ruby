457b411248aae7d18ba3fbcb1afe61156440270a
Sky Blue <dawadiaakash@gmail.com>
2025-07-03 11:18:57 +0545

    Add basic ruby code to repo(Ruby_p1)

---
+++ Ruby_p1/part6.rb
@@ -0,0 +1,23 @@
+# Hashes: collection of key value pairs
+
+state = {
+    "BHARATPUR" => "BNP",
+    "KATHMANDU" => "KTM",
+    "POKHARA" => "PKR",
+    :JANAKPUR => "JKR",
+    22 => "Twenty Two",
+    }
+
+    # puts state
+    # puts state["BHARATPUR"]
+    # puts state[:JANAKPUR]
+    # puts state[22]
+
+
+
+# Methods: block of code that can be called
+
+def greet 
+  puts "Hello, Elton John!"
+end
+greet 
